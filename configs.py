4089bb78ad90265033f0c2149d83a56525aec380
𝘿𝙊𝙉𝘼𝙏𝙀 ๛ 𝘼𝙍𝙈𝙔 🇮🇳 ™ <sanjoymandal.902137@gmail.com>
2024-12-27 12:08:34 +0530

    𝗗𝗢𝗡𝗔𝗧𝗘_𝗔𝗥𝗠𝗬_𝗕𝗢𝗧𝗦 ™

--- configs.py
+++ configs.py
@@ -1,15 +1,19 @@
-from dotenv import load_dotenv
-from os import getenv
-
-# Load environment variables from .env file
-load_dotenv()
+from os import path, getenv
 
 class Config:
-    API_ID = getenv("API_ID", "")
+    API_ID = int(getenv("API_ID", ""))
     API_HASH = getenv("API_HASH", "")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
+    
+    # List of required channels, each with an ID and link
+    REQUIRED_CHANNELS = [
+        {"id": int(getenv("CHANNEL_1_ID", "")), "link": getenv("CHANNEL_1_LINK", "")},
+        {"id": int(getenv("CHANNEL_2_ID", "")), "link": getenv("CHANNEL_2_LINK", "")},
+        {"id": int(getenv("CHANNEL_3_ID", "")), "link": getenv("CHANNEL_3_LINK", "")}
+        # Add more channels as needed
+    ]
+    
     SUDO = list(map(int, getenv("SUDO", "").split()))
     MONGO_URI = getenv("MONGO_URI", "")
-    REQUIRED_CHANNELS = getenv("REQUIRED_CHANNELS", "").split()  # Multiple channels
-
+    
 cfg = Config()
