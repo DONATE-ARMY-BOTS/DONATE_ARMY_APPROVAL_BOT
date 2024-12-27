511f369ac74629278c3d7df2d0715341d1da208c
𝘿𝙊𝙉𝘼𝙏𝙀 ๛ 𝘼𝙍𝙈𝙔 🇮🇳 ™ <sanjoymandal.902137@gmail.com>
2024-12-27 09:55:14 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,13 +1,11 @@
-
-from os import path, getenv
+from os import getenv
 
 class Config:
     API_ID = getenv("API_ID", "")
     API_HASH = getenv("API_HASH", "")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
-    FSUB = getenv("FSUB", "")
-    CHID = getenv("CHID", "")
     SUDO = list(map(int, getenv("SUDO", "").split()))
     MONGO_URI = getenv("MONGO_URI", "")
-    
+    REQUIRED_CHANNELS = getenv("REQUIRED_CHANNELS", "").split()  # Multiple channels
+
 cfg = Config()
