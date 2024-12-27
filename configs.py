d162bb9a2e4b04fda24cd42bc8f5ed1176803ba7
𝘿𝙊𝙉𝘼𝙏𝙀 ๛ 𝘼𝙍𝙈𝙔 🇮🇳 ™ <sanjoymandal.902137@gmail.com>
2024-12-27 09:36:03 +0530

    𝗗𝗢𝗡𝗔𝗧𝗘_𝗔𝗥𝗠𝗬_𝗕𝗢𝗧𝗦 ™

--- configs.py
+++ configs.py
@@ -2,11 +2,11 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", ""))
-    API_HASH = int(getenv("API_HASH", ""))
-    BOT_TOKEN = int(getenv("BOT_TOKEN", ""))
-    FSUB = int(getenv("FSUB", ""))
-    CHID = int(getenv("CHID", ""))
+    API_ID = getenv("API_ID", "")
+    API_HASH = getenv("API_HASH", "")
+    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    FSUB = getenv("FSUB", "")
+    CHID = getenv("CHID", "")
     SUDO = list(map(int, getenv("SUDO", "").split()))
     MONGO_URI = getenv("MONGO_URI", "")
     
